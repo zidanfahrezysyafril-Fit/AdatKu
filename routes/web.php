d357a4052ae19e65fe0b0e967bdaff9c5465de25
ZidanFahrezyS <zidanfahrezysyafril@gmail.com>
2025-11-10 14:02:18 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -35,10 +35,6 @@ Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
-<<<<<<< HEAD
-    })->name('dashboard');
-});
-=======
     })->name('dashboard.auth');
 });
->>>>>>> 81bdab28264c82ea398382c567f119fe33284aa4
+
