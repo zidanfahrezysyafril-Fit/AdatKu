66334081f9b584ec382bfb7428bc0a6394901a58
fettyratna <fettyratna29@gmail.com>
2025-11-10 22:08:18 +0700

    knk

--- app/Models/Layanan.php
+++ app/Models/Layanan.php
@@ -2,25 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Layanan extends Model
 {
-    use HasFactory;
-    protected $primaryKey = 'Id_Layanan';
-    protected $fillable = [
-        'Id_Mua', 'Id_Pengguna', 'Nama_Layanan', 'Kategori',
-        'Deskripsi', 'Ukuran_Status', 'Harga'
-    ];
+    protected $guarded = [];
+    protected $table  = 'layanan';
 
     public function mua()
     {
-        return $this->belongsTo(Mua::class, 'Id_Mua');
-    }
-
-    public function pengguna()
-    {
-        return $this->belongsTo(Pengguna::class, 'Id_Pengguna');
+        return $this->belongsTo(Mua::class, 'mua_id');
     }
 }
